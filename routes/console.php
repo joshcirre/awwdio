9d7f361cc9c5c03043f8a6bee8fe1cbcd29e1123
Josh Cirre <josh@Joshs-Larabook.local>
2024-07-20 22:07:12 -0700

    scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Models\ListeningParty;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// set schedule to run every minute to set completed listening parties to inactive
+Schedule::call(function () {
+    ListeningParty::where('end_time', '<=', now())->update(['is_active' => false]);
+})->everyMinute();
